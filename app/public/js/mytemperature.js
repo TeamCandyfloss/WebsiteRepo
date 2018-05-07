1cee15b422dfd5cdf3d2a9559b2a3601942c4592
NikoDyring <Nikodyring@gmail.com>
2018-05-07 16:02:04 +0200

    Nodejs & ExpressJs

--- app/public/js/mytemperature.js
+++ app/public/js/mytemperature.js
@@ -1,3 +1,21 @@
-$.get("INSERT_URL/api/id", {ID=1}, function(temperature){
-  alert("Data loaded: " + temperature);
+$(function() {
+
+    var output = '';
+
+    $.ajax({
+        type : 'GET',
+        dataType: 'jsonp',
+        url: 'http://localhost:57019/Service1.svc/Temperatures/',
+        success: showTemperatures
+        });
+
+        function showTemperatures(data) {
+     output += '<h1>Previous Temperatures</h1>';
+    $.each(data,function(key, item) {
+        output += '<h1> ' + item.Place +'  </h1>';
+          output += '<h3> ' + item.Temperature + '°' + '</h3>';
+
+    });
+    $('.temperatures').html(output);
+}
 });
