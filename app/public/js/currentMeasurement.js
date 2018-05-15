e3dab6472ccfed4ee07bf3295b9d41f0b7425598
Allan Holm Søndergard <allan_2233@hotmail.com>
2018-05-15 09:34:36 +0200

    convertere til temp + UI til temp

--- app/public/js/currentMeasurement.js
+++ app/public/js/currentMeasurement.js
@@ -14,7 +14,73 @@ $(function() {
      output += '<h1>' + data.Place + '</h1>';
      output += '<h3>' + data.Temperature + '</h3>';
      output += '<h3>' + data.Time + '</h3>';
-     currentTemp = data.Temperature;
     $('.currentTemperature').html(output);
-}
+    }
 });
+
+function celsius(){
+$(function() {
+
+    var output = '';
+
+    $.ajax({
+        type : 'GET',
+        dataType: 'jsonp',
+        url: 'http://pleaseworknow.azurewebsites.net/service1.svc/currentTemp/',
+        success: showTemperature
+        });
+
+        function showTemperature(data) {
+     output += '<h1>Current Temperature</h1>';
+     output += '<h1>' + data.Place + '</h1>';
+     output += '<h3>' + data.Temperature + '</h3>';
+     output += '<h3>' + data.Time + '</h3>';
+    $('.currentTemperature').html(output);
+    }
+});
+}
+
+function kelvin(){
+    $(function() {
+    
+        var output = '';
+    
+        $.ajax({
+            type : 'GET',
+            dataType: 'jsonp',
+            url: 'http://pleaseworknow.azurewebsites.net/service1.svc/currentTemp/',
+            success: showTemperature
+            });
+    
+            function showTemperature(data) {
+         output += '<h1>Current Temperature</h1>';
+         output += '<h1>' + data.Place + '</h1>';
+         output += '<h3>' + (data.Temperature+273.15) + '</h3>';
+         output += '<h3>' + data.Time + '</h3>';
+        $('.currentTemperature').html(output);
+    }
+    });
+}
+
+function fahrenheit(){
+    $(function() {
+    
+        var output = '';
+    
+        $.ajax({
+            type : 'GET',
+            dataType: 'jsonp',
+            url: 'http://pleaseworknow.azurewebsites.net/service1.svc/currentTemp/',
+            success: showTemperature
+            });
+    
+            function showTemperature(data) {
+         output += '<h1>Current Temperature</h1>';
+         output += '<h1>' + data.Place + '</h1>';
+         output += '<h3>' + ((data.Temperature*1.8)+32) + '</h3>';
+         output += '<h3>' + data.Time + '</h3>';
+        $('.currentTemperature').html(output);
+    }
+    });
+}
+
