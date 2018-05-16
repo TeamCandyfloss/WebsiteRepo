475a1961e83c82f3ca581916703b92f4fb0ba550
Fridai <Nikolaiviby3@gmail.com>
2018-05-16 11:55:33 +0200

    currentplace post virker nu

--- app/public/js/currentPlace.js
+++ app/public/js/currentPlace.js
@@ -7,33 +7,33 @@ $(function() {
     //       Place: $('.Place').val()
     //     } );
     //   });
-    var place = $('.Place').val();
-    var userData = ""+ place +"";
+    
    // 'localhost:57019/service1.svc/temperatures/PostPlace',
 
     $('#SubmitBtn').click( function(e) {
+        var place = $('#placex').val();
+    var userData = {"Place":place};
         e.preventDefault();
         $.ajax({
             url: 'http://pleaseworknow.azurewebsites.net/service1.svc/temperatures/PostPlace',
             type: 'post',
-            dataType: 'jsonp',
-            data: userData,
-            success: function(data) {
-       alert(data);
-                     }
-       });
+            dataType: 'json',
+            contentType: 'application/json',
+            //encoding: 'Encoding.UTF8',
+            data: JSON.stringify(userData), //'"virknu"',
+            
+    //    });
         // $("#SubmitBtn").click(function(){        
-        //     $.post("http://pleaseworknow.azurewebsites.net/service1.svc/temperatures/PostPlace",{ 
-        //    $(userData)}, 
+        //     $.post("http://pleaseworknow.azurewebsites.net/service1.svc/temperatures/PostPlace",JSON.stringify(userData), 
         //     function(data) {
         //         alert(data);
         //     });
         //  });
     // $('.Place-Form').submit(function(e) {
-    //     //e.preventDefault();
+    //     e.preventDefault();
     //     $.post('http://pleaseworknow.azurewebsites.net/service1.svc/temperatures/PostPlace', {
-    //       place: $('#Place').val()
+    //       place: $('.Place').val()
     //     });
-    // });
+     });
    });
 });
