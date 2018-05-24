3838905ae4b1a72ce3a701347af49dc86d829b30
NikoDyring <Nikodyring@gmail.com>
2018-05-24 14:39:23 +0200

    Styled delete button

--- app/public/js/delete.js
+++ app/public/js/delete.js
@@ -17,7 +17,7 @@ $(function () {
         output += '<th>Temperature</th>';
         output += '<th>Location</th>';
         output += '<th>Date</th>';
-        output += '<td></td>';
+        output += '<th>Delete</th>';
         output += '</tr>';
 
         $.each(data, function (key, item) {
@@ -25,10 +25,10 @@ $(function () {
 
             output += '<tr>';
             output += '<th>' + item.ID + '</th>';
-            output += '<td>' + item.Temperature + '</td>';
+            output += '<td>' + item.Temperature + '°</td>';
             output += '<td>' + item.Place + '</td>';
             output += '<td>' + item.Date + '</td>';
-            output += `<td> <div class="media-left"><button id='${item.ID}' class="feedback-delete btn btn-xs btn-danger"><span id="' + item.Id + '" class="glyphicon glyphicon-remove"></span></button></div> </td>`;
+            output += `<td> <div class="media-left"><button id='${item.ID}' class="delete-btn btn btn-xs btn-danger">Delete<span id="' + item.Id + '" class="glyphicon glyphicon-remove"></span></button></div> </td>`;
             output += '</tr>';
 
 
@@ -37,7 +37,7 @@ $(function () {
         $('#temperatures').html(output);
     };
 
-    $(document).on("click", '.feedback-delete', function (e) {
+    $(document).on("click", '.delete-btn', function (e) {
         var deleteurl = 'http://pleaseworknow.azurewebsites.net/service1.svc/temperatures/';
         var deleteData = {'ID':e.target.id};
         $.ajax({
